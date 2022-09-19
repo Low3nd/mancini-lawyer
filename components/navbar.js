caf8bd57d3c9424af91df241038384194ba33f48
low3nd <nicoschuh@hotmail.fr>
2022-09-19 18:44:31 +0200

    Created custom Document
    set up the Navbar component
    
    Created the Layout component

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Navbar () {
+    return (
+        <nav className="navigation">
+            <p>Boom</p>
+        </nav>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Head>
+        <title>Maître MANCINI - Avocate à Tours</title>
+      </Head>
+      <h1>Hello World</h1>
+    </div>
   )
 }
