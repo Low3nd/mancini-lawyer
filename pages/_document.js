7b1fc9d2a17fe5a850e04cb1f531b0c7e0612edb
low3nd <nicoschuh@hotmail.fr>
2022-09-20 17:42:18 +0200

    Implemented the navigation bar

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&family=Raleway:wght@400;500;700&display=swap" rel="stylesheet" /> 
       </Head>
       <body>
         <Main />
